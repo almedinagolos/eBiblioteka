1b60da267e8051525cabdd7b9ef6e410ce836f03
Almedina <almedina.golos@edu.fit.ba>
2022-01-14 13:08:15 +0100

    update

--- eBiblioteka/eBiblioteka/Migrations/20220114120356_novipodaciubazi.Designer.cs
+++ eBiblioteka/eBiblioteka/Migrations/20220114120356_novipodaciubazi.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eBiblioteka.DB;
 
 namespace eBiblioteka.Migrations
 {
     [DbContext(typeof(MojDbContext))]
-    partial class MojDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220114120356_novipodaciubazi")]
+    partial class novipodaciubazi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
